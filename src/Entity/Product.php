55df420e68def80e91a72baca7a874c1b80fa9ab
miguelalbox <49124664+miguelalbox@users.noreply.github.com>
2022-09-13 12:59:35 +0200

    [Features] Create Entity Product #3

---
+++ src/Entity/Product.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $brand = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $model = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $processor = null;
+
+    #[ORM\Column]
+    private ?int $screensize = null;
+
+    #[ORM\Column]
+    private ?int $battery = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): self
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getProcessor(): ?string
+    {
+        return $this->processor;
+    }
+
+    public function setProcessor(string $processor): self
+    {
+        $this->processor = $processor;
+
+        return $this;
+    }
+
+    public function getScreensize(): ?int
+    {
+        return $this->screensize;
+    }
+
+    public function setScreensize(int $screensize): self
+    {
+        $this->screensize = $screensize;
+
+        return $this;
+    }
+
+    public function getBattery(): ?int
+    {
+        return $this->battery;
+    }
+
+    public function setBattery(int $battery): self
+    {
+        $this->battery = $battery;
+
+        return $this;
+    }
+}
